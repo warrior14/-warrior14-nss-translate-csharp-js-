e962cbac21da1b7b04a1db0cd5e335849b23cabf
warrior14 <ironaddicts14@gmail.com>
2021-08-24 15:32:10 -0500

    translated js fizzbuzz exercise into csharp

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,27 @@
+using System; 
+
+
+void Main() 
+{
+    for (int n = 1; n <= 100; n++) 
+    {
+        if (n % 15 === 0)
+        {
+            Console.WriteLine("fizzbuzz");
+        }
+        else if (n % 3 === 0)
+        {
+            Console.WriteLine("fizz");
+        }
+        else if (n % 5 === 0);
+        {
+            Console.WriteLine("buzz");
+        }
+        else 
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
+
+Main();
